bbe9163565eccb7710d53657c1965529e4f188aa
Unknown <stcyliax@gmail.com>
2018-02-20 21:34:42 +0100

    Initial commit.
    The car should drive on a fixed offset to the double yellow

--- src/main.cpp
+++ src/main.cpp
@@ -247,6 +247,19 @@ int main() {
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
+						double dist_inc = 0.4;
+						for (int i = 0; i < 50; i++ )
+						{
+							double next_s = car_s + (i+1)*dist_inc;
+							double next_d = 6.0;
+							vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+							next_x_vals.push_back(xy[0]);
+							next_y_vals.push_back(xy[1]);
+						}
+
+
+
           	auto msg = "42[\"control\","+ msgJson.dump()+"]";
 
           	//this_thread::sleep_for(chrono::milliseconds(1000));
